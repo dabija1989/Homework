3e49bcbe089489fa9080942f9d6d0e8d6b75791c
dabija1989 <dabija.dmitri@gmail.com>
2023-06-02 18:00:10 +0300

    home work

---
+++ homehork/ex1.py
@@ -0,0 +1,5 @@
+# Completeaza codul de mai jos pentru a
+# afișa numele utilizatorului introdus la consola.
+
+username = "Dmitri"
+print("Welcome " + username)
---
+++ homehork/ex3.py
@@ -0,0 +1,13 @@
+# Creaza un program care va cere de la utilizator:
+#
+# Adresa de email
+# Numele de utilizator
+# Și va afisa aceasta infromatie in urmatorul format:
+#
+# Emailul pentru Marius a fost expediat cu succes pe adresa de email mariustricolici@hotmail.com
+#
+# Pentru adresa de email: mariustricolici@hotmail.com Si numele de utilizator: Marius
+
+Adresa_email = input("Va rog introduceti adresa dumneavoastra de email:")
+Nume_utilizator = input("Va rog introduceti numele dumneavoasta de utilizator:")
+print("Emailul pentru " + Nume_utilizator + "a fost expediat cu succes pe adresa de email " + Adresa_email)
